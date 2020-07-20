84fd5a511c09ce1808a8c5a1c1aae526032a2eea
shelleychenn <shelleychen322@gmail.com>
2020-07-20 00:09:08 -0400

    Refactor controller functions into controllers and models

--- server/src/controller.js
+++ server/src/controller.js
@@ -1,84 +1,46 @@
-const pool = require('./pg');
-const Cursor = require('pg-cursor');
+const models = require('./model');
 
 module.exports = {
-  getAllProducts: async function (req, res) {
-    try {
-      const client = await pool.connect();
-      const queryStr = 'SELECT * FROM product';
-      const cursor = await client.query(new Cursor(queryStr));
-
-      cursor.read(10, (err, rows) => {
-        console.log('we got the first 10 rows');
-        console.log(rows);
-        res.status(200).json(rows);
-      });
-    } catch (err) {
-      console.log(err);
-      res.sendStatus(500);
-    }
-  },
-
-  getProductInfo: async function (req, res) {
-    let productId = req.params.product_id;
-    try {
-      const client = await pool.connect();
-      const queryStr = `SELECT distinct * FROM product WHERE id=${productId}`;
-      const data = client.query(queryStr);
-      const featureQueryStr = `SELECT feature, value FROM features WHERE productid=${productId}`;
-      const featureData = client.query(featureQueryStr);
-      const promises = await Promise.all([data, featureData]);
-      const [product, features] = promises;
-      if (product.rowCount > 0) {
-        const productInfo = product.rows[0];
-        productInfo.features = features.rows;
-        res.status(200).json(productInfo);
+  getAllProducts: function (req, res) {
+    models.getAllProductsModel((err, allProducts) => {
+      if (err) {
+        res.sendStatus(500);
       } else {
-        res.sendStatus(404);
+        res.status(200).json(allProducts);
       }
-    } catch (err) {
-      console.log(err);
-      res.sendStatus(500);
-    }
+    });
   },
 
-  // FIXME:
-  getProductStyles: async function (req, res) {
+  getProductInfo: function (req, res) {
     let productId = req.params.product_id;
-    let returnObj = {
-      product_id: productId,
-    };
-    try {
-      const client = await pool.connect();
-      const queryStr = `SELECT style_id, name, original_price, sale_price, "default?" FROM styles WHERE productid=${productId}`;
-      const data = await client.query(queryStr);
-      let allStyles = data.rows;
-      for (let i = 0; i < allStyles.length; i++) {
-        const skusQueryStr = `SELECT size, quantity FROM skus WHERE styleId=${allStyles[i].style_id}`;
-        const skusData = await client.query(skusQueryStr);
-        allStyles[i].skus = skusData.rows;
+    models.getProductInfoModel(productId, (err, productInfo) => {
+      if (err) {
+        res.sendStatus(500);
+      } else {
+        res.status(200).json(productInfo);
       }
-      returnObj.results = allStyles;
-      res.status(200).json(returnObj);
-    } catch (err) {
-      console.log(err);
-    }
+    });
   },
 
-  getRelatedProducts: async function (req, res) {
+  getProductStyles: function (req, res) {
     let productId = req.params.product_id;
-    try {
-      const client = await pool.connect();
-      const queryStr = `SELECT related_product_id FROM related_products WHERE current_product_id=${productId}`;
-      const data = await client.query(queryStr);
-      let idObjects = data.rows;
-      let ids = idObjects.map((idObject) => {
-        return idObject.related_product_id;
-      });
-      res.status(200).json(ids);
-    } catch (err) {
-      console.log(err);
-      res.sendStatus(500);
-    }
+    models.getProductStylesModel(productId, (err, styleObj) => {
+      if (err) {
+        res.sendStatus(500);
+      } else {
+        res.status(200).json(styleObj);
+      }
+    });
+  },
+
+  getRelatedProducts: function (req, res) {
+    let productId = req.params.product_id;
+    models.getRelatedProductsModel(productId, (err, ids) => {
+      if (err) {
+        res.sendStatus(500);
+      } else {
+        res.status(200).json(ids);
+      }
+    });
   },
 };
---
+++ server/src/model.js
@@ -0,0 +1,79 @@
+const pool = require('./pg');
+const Cursor = require('pg-cursor');
+
+module.exports = {
+  getAllProductsModel: async function (callback) {
+    try {
+      const client = await pool.connect();
+      const queryStr = 'SELECT * FROM product';
+      const cursor = await client.query(new Cursor(queryStr));
+      return cursor.read(10, (err, rows) => {
+        if (err) {
+          callback(err, null);
+        } else {
+          callback(null, rows);
+        }
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+
+  getProductInfoModel: async function (productId, callback) {
+    try {
+      const client = await pool.connect();
+      const queryStr = `SELECT distinct * FROM product WHERE id=${productId}`;
+      const data = client.query(queryStr);
+      const featureQueryStr = `SELECT feature, value FROM features WHERE productid=${productId}`;
+      const featureData = client.query(featureQueryStr);
+      const promises = await Promise.all([data, featureData]);
+      const [product, features] = promises;
+      if (product.rowCount > 0) {
+        const productInfo = product.rows[0];
+        productInfo.features = features.rows;
+        callback(null, productInfo);
+      } else {
+        callback(err, null);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  },
+
+  // FIXME:
+  getProductStylesModel: async function (productId, callback) {
+    let returnObj = {
+      product_id: productId,
+    };
+    try {
+      const client = await pool.connect();
+      const queryStr = `SELECT style_id, name, original_price, sale_price, "default?" FROM styles WHERE productid=${productId}`;
+      const data = await client.query(queryStr);
+      let allStyles = data.rows;
+      for (let i = 0; i < allStyles.length; i++) {
+        const skusQueryStr = `SELECT size, quantity FROM skus WHERE styleId=${allStyles[i].style_id}`;
+        const skusData = await client.query(skusQueryStr);
+        allStyles[i].skus = skusData.rows;
+      }
+      returnObj.results = allStyles;
+      callback(null, returnObj);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+
+  getRelatedProductsModel: async function (productId, callback) {
+    try {
+      const client = await pool.connect();
+      const queryStr = `SELECT related_product_id FROM related_products WHERE current_product_id=${productId}`;
+      const data = await client.query(queryStr);
+      let idObjects = data.rows;
+      let ids = idObjects.map((idObject) => {
+        return idObject.related_product_id;
+      });
+      callback(null, ids);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+};
