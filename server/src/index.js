558dd70e63bc759b295266aaa076d7cb98d2b73d
shelleychenn <shelleychen322@gmail.com>
2020-07-21 22:48:02 -0400

    Change port number

--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const router = require('./router');
 
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
