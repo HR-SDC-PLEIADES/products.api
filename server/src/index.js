f48dff0cd0ece8a627d2efdf5516e97fbb2cfc80
shelleychenn <shelleychen322@gmail.com>
2020-07-23 20:26:59 -0400

    Refactor Express server to run on multiple CPU cores using Cluster

--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,41 @@
 const express = require('express');
+const os = require('os');
+const cluster = require('cluster');
 const bodyParser = require('body-parser');
 const router = require('./router');
 const cors = require('cors');
-const app = express();
 const PORT = 3030;
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+const clusterWorkerSize = os.cpus().length;
 
-app.use('/', router);
+if (clusterWorkerSize > 1) {
+  if (cluster.isMaster) {
+    for (let i = 0; i < clusterWorkerSize; i++) {
+      cluster.fork();
+    }
 
-app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
-});
+    cluster.on('exit', (worker) => {
+      console.log('Worker', worker.id, ' has exitted!');
+    });
+  } else {
+    const app = express();
+    app.use(cors());
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use('/', router);
+    app.listen(PORT, () => {
+      console.log(`listening on port ${PORT} and worker ${process.pid}`);
+    });
+  }
+} else {
+  const app = express();
+  app.use(cors());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use('/', router);
+  app.listen(PORT, () => {
+    console.log(
+      `listening on port ${PORT} with the single worker ${process.pid}`
+    );
+  });
+}
